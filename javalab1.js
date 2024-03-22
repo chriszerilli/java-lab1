b1267e9134d1745273f9e68e61010119696a411b
chriszerilli <157649570+chriszerilli@users.noreply.github.com>
2024-03-21 22:18:21 -0400

    Add files via upload

---
+++ javalab1.js
@@ -0,0 +1,59 @@
+document.body.innerHTML= "Javascript Lab 1";
+//question 1
+let num1 = 10;
+let num2 = 5;
+
+let addition= num1 + num2;
+let subtrction= num1 - num2;
+let multiplication= num1 * num2;
+let division= num1 / num2;
+
+console.log("addition:", addition);
+console.log("subtrction:", subtrction);
+console.log("multiplication:", multiplication);
+console.log("division:", division);
+
+//question2
+
+let firstname = "chris";
+let lastname = "zerilli";
+
+let fullname = firstname + " " + lastname
+
+console.log("full name:",fullname);
+
+//question 3
+let x = 5;
+var y = 10;
+
+console.log("x:",x);
+console.log("y:",y);
+
+//question4
+let counter = 0;
+counter += 1;
+counter += 1;
+counter += 1;
+counter += 1;
+counter += 1;
+
+console.log("final counter value:",counter);
+
+//question 5
+let numstring1 = prompt("enter number 1");
+let numstring2 = prompt("enter number 2");
+
+let num1converted = +numstring1;
+let num2converted = +numstring2;
+
+let sum = num1converted + num2converted;
+
+console.log("the sum:", sum);
+
+let age = 25;
+let ageString = age.toString() + "years old"
+
+console.log("Age String:", ageString);
+
+
+
